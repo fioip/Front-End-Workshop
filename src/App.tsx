3f8d071604940cab4e399fbfee1fed87a8c55a30
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2025-03-20 17:01:33 +0200

    Installed MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return;
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>}></Route>
-        <Route path="/search" element={<h1>Search Page</h1>}></Route>
-        <Route path="/individual" element={<h1>Individual Page</h1>}></Route>
-      </Routes>
-    </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1></h1>}></Route>
+          <Route path="/search" element={<h1>Search Page</h1>}></Route>
+          <Route path="/individual" element={<h1>Individual Page</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+// import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="container">
+      <ul>
+        <li>Home Page</li>
+        <li>Search Page</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
