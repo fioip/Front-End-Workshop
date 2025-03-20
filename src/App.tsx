a96f4d2ba66d413a1ea78214c4c94dce1c6826ce
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2025-03-20 16:44:14 +0200

    Routes of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Router, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return;
   <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={}></Route>
-        <Route path="/" element={}></Route>
-        <Route path="/" element={}></Route>
+        <Route path="/" element={<h1>Home Page</h1>}></Route>
+        <Route path="/search" element={<h1>Search Page</h1>}></Route>
+        <Route path="/individual" element={<h1>Individual Page</h1>}></Route>
       </Routes>
     </BrowserRouter>
   </>;
