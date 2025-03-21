ec6ae4b036b30eccb3de54dc79cd5a9b66a43120
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2025-03-21 14:40:12 +0200

    Navbar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/HomePage/Home";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index path="/home" element={<Home />}></Route>
-          <Route path="/" element={<Navbar />}></Route>
           <Route path="/search" element={<h1>Search Page</h1>}></Route>
           <Route path="/individual" element={<h1>Individual Page</h1>}></Route>
         </Routes>
