5d9c0ba66984c8860ec9607a85e71a98d468006f
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2025-03-21 14:44:47 +0200

    feat: Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Home from "./components/HomePage/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1></h1>}></Route>
+          <Route index path="/home" element={<Home />}></Route>
           <Route path="/search" element={<h1>Search Page</h1>}></Route>
           <Route path="/individual" element={<h1>Individual Page</h1>}></Route>
         </Routes>
       </BrowserRouter>
-      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+import Container from "@mui/material/Container";
+// import Button from "@mui/material/Button";
+import { NavLink } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+const pages = [
+  { pathName: "Home Page", url: "/home" },
+  { pathName: "Search Page", url: "/search" },
+];
+
+function Navbar() {
+  //   const nav = useNavigate();
+
+  //   const navigate = (path: string) => {
+  //     console.log(path);
+  //     nav(path);
+  //   };
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#2ecc71" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/home"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Poppins",
+              fontWeight: 800,
+              letterSpacing: ".2rem",
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Song Finder
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            {pages.map((page) => (
+              //   <Button
+              //     key={page.url}
+              //     sx={{ my: 2, color: "white", display: "block" }}
+              //     onClick={() => navigate(page.url)}
+              //   >
+              //     {page.pathName}
+              //   </Button>
+              <NavLink to={page.url} style={{ textDecoration: "none" }}>
+                <Typography variant="h6" color="white">
+                  {page.pathName}
+                </Typography>
+              </NavLink>
+            ))}
+          </Stack>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
