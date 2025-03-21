2abc7f50e12ae98b22fae712589ee45cb7a7513d
Oros Razvan Andrei <oros.razvanandrei@student.uoradea.ro>
2025-03-21 14:31:23 +0200

    added individualpage component to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import IndividualPage from "./components/IndividualPage/IndividualPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<h1></h1>}></Route>
           <Route path="/search" element={<h1>Search Page</h1>}></Route>
-          <Route path="/individual" element={<h1>Individual Page</h1>}></Route>
+          <Route path="/individual" element={<IndividualPage />}></Route>
         </Routes>
       </BrowserRouter>
-      <Navbar />
     </>
   );
 }
